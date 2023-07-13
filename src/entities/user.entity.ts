a27eadc42f0676786102ede289f8478eedbec8f4
Teruhiko Shibuya <shibuya-teruhiko@mki.co.jp>
2023-07-13 18:51:04 +0900

    add relation

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Item } from "./item.entity";
 
 @Entity()
 export class User {
@@ -13,4 +14,10 @@ export class User {
 
   @Column()
   status: string;
+
+  // リレーション用
+  //   ユーザは複数のアイテムを持つことができる(User:Item = 1:many)
+  // OneToMany()引数：関連先の型と、関連先で紐づけられるプロパティを返すコールバック関数を指定
+  @OneToMany(() => Item, (item) => item.user) 
+  items: Item[];
 }
