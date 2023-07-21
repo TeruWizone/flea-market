844828e987dc399d216a2a64c4af3558138957ed
Teruhiko Shibuya <shibuya-teruhiko@mki.co.jp>
2023-07-21 19:06:49 +0900

    ssl

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./ssl/exidsvrnonkey.pem'),
+    cert: fs.readFileSync('./ssl/exidsvrcrt.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {httpsOptions});
   app.useGlobalPipes(new ValidationPipe)
   await app.listen(3000);
 }
