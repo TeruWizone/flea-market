0e94a7a014d98a9c5160719098653893339d5f26
Teruhiko Shibuya <shibuya-teruhiko@mki.co.jp>
2023-07-28 10:19:47 +0900

    sql string

--- src/logtbls/logtbls.service.ts
+++ src/logtbls/logtbls.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { LogtblRepository } from './logtbl.repository';
 import { LogTbl } from 'src/entities/log_tbl.entity';
-import { Like, SelectQueryBuilder, getConnection, getManager, getRepository } from 'typeorm';
-import { isNotEmpty } from 'class-validator';
 
 @Injectable()
 export class LogtblsService {
@@ -15,19 +13,17 @@ export class LogtblsService {
   }
 
   async findAll(): Promise<LogTbl[]> {
-    const result = await this.logtblRepository
-      .query(
-        `select * 
-        from log_tbl
-        where tag <> 'get_pid_trigger_2023_07_14_19_48_11_574439' 
-          and ((tag like '%spx%')) 
-          and ((log_date between '2023-07-01 00:00' and '2023-07-31 23:59')) 
-          and ( log_data->>'record'::text like '%test%') 
-        order by log_date`
-      )
+    const sql =
+      "select * \
+        from log_tbl \
+        where tag <> 'get_pid_trigger_2023_07_14_19_48_11_574439' \
+          and ((tag like '%spx%')) \
+          and ((log_date between '2023-07-01 00:00' and '2023-07-31 23:59')) \
+          and ( log_data->>'record'::text like '%test%') \
+        order by log_date";
 
+    const result = await this.logtblRepository.query(sql);
     console.log(result);
     return result;
   }
-
 }
